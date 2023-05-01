6b682093a9fd0900d67afdf53cb3c62ac937a730
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-05-01 21:54:26 +0300

    added docker-compose; added db port

--- database.ts
+++ database.ts
@@ -8,10 +8,12 @@ class Database {
         const dbLogin = process.env.DB_LOGIN || "login";
         const dbPass = process.env.DB_PASSWORD || "pass";
         const db_host = process.env.DB_HOST_ADDRES || "localhost";
+        const db_port = process.env.DB_PORT || "3306";
         //database       
         Database.sequelize = new Sequelize(database, dbLogin, dbPass, {
             dialect: "mysql",
             host: db_host,
+            port: +db_port,
             define: {
                 timestamps: false
             }
